571650e4e7d59119cee3db46c4460a465eb6ef5c
aroary <meanchemshkedi@gmail.com>
2022-04-21 18:23:57 -0400

    tests: cpu use plot membery uses bar

--- tests/cpu.js
+++ tests/cpu.js
@@ -1,5 +1,5 @@
 const os = require('os');
 const processCommand = require("../index.js");
 
-os.cpus().forEach((cpu, id) => id < 4 && processCommand(`--graph CPU_${id} bar ${cpu.model} ${cpu.speed}`));
+os.cpus().forEach((cpu, id) => id < 4 && processCommand(`--graph CPU_${id} plot ${cpu.model} ${cpu.speed}`));
 setInterval(() => os.cpus().forEach((cpu, id) => id < 4 && processCommand(id + " " + cpu.times.user)), 1000);
