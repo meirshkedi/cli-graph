73b1d49a166f7e1b3658f29787f85c885f0d22a8
aroary <meanchemshkedi@gmail.com>
2022-04-21 16:28:08 -0400

    dynamic cimentions

--- graphs/bar.js
+++ graphs/bar.js
@@ -1,18 +1,18 @@
-function bar(items, output, left) {
-    if (items.length > 40) items.shift();
-    var data = new Array(5).fill("");
-    var lowest = Math.floor(Math.min(...items));
-    var highest = Math.floor(Math.max(...items));
+function bar(graph, output, left) {
+    if (graph.data.length > graph.width) graph.data.shift();
+    var data = new Array(graph.height).fill("");
+    var lowest = Math.floor(Math.min(...graph.data));
+    var highest = Math.floor(Math.max(...graph.data));
     var range = highest - lowest;
-    var row = range / 5;
-    items.forEach((value, index) => {
+    var row = range / graph.height;
+    graph.data.forEach((value, index) => {
         var height = (value - lowest) / row;
-        for (var i = 0; i < 5; i++) {
+        for (var i = 0; i < graph.height; i++) {
             if (i <= height) data[i] += "█";
             else data[i] += "┼";
         }
     });
-    data.reverse().map(value => value.length < 40 ? "┼".repeat(40 - value.length) + value : value).forEach((value, index) => {
+    data.reverse().map(value => value.length < graph.width ? "┼".repeat(graph.width - value.length) + value : value).forEach((value, index) => {
         left[index + 3] = ["╞", (Math.round(row) * (data.length - index) + lowest).toString(), "╡"];
         output[index + 3] = value.replace(/█+/g, "\x1b[36m$&").replace(/┼+/g, "\x1b[35m$&") + "\x1b[0m│";
     });
--- index.js
+++ index.js
@@ -16,33 +16,33 @@ flow.on("data", (id, item) => {
 
     screen.forEach((graph, id) => {
         if (!graph.data.length) return;
-        var output = new Array(10).fill("");
-        var left = new Array(10).fill([]);
+        var output = new Array(graph.height + 5).fill("");
+        var left = new Array(graph.height + 5).fill([]);
 
         // Initiate left side
         left[1] = ["─", "ABCD"[id], "┌"];
         left[2] = ["╞", "═", "╤"];
-        left[8] = ["╘", "═", "╧"];
+        left[graph.height + 3] = ["╘", "═", "╧"];
 
         // Format output
         output[1] += `─\x1b[32m${graph.name}\x1b[0m─┐`;
-        output[2] += `${"═".repeat(graph.name.length + 2)}╧${"═".repeat(37 - graph.name.length)}╕`;
-        output[8] += "═".repeat(40) + "╛";
+        output[2] += `${"═".repeat(graph.name.length + 2)}╧${"═".repeat(graph.width - 3 - graph.name.length)}╕`;
+        output[graph.height + 3] += "═".repeat(graph.width) + "╛";
 
         if (graph.type === "bar") {
             // Format bar graph
-            var data = require("./graphs/bar")(graph.data, output, left);
+            var data = require("./graphs/bar")(graph, output, left);
             output = data.output;
             left = data.left;
         };
 
         // Format left side
         var width = Math.max(...left.map(value => value.join``.length - 2));
-        left.forEach((value, index) => index > 0 && index < 9 && (left[index] = `${left[index][0]}${(index === 1 ? "─" : "═").repeat(width - left[index][1].length)}${left[index].slice(1).join``}`));
+        left.forEach((value, index) => index > 0 && index < graph.height + 4 && (left[index] = `${left[index][0]}${(index === 1 ? "─" : "═").repeat(width - left[index][1].length)}${left[index].slice(1).join``}`));
         left[1] = left[1].split``.reverse().join``.replace(/[a-z]+/i, "\x1b[32m$&\x1b[0m");
-        for (var i = 0; i < 5; i++) left[i + 3] = left[i + 3].replace(/\d+/g, "\x1b[33m$&\x1b[0m");
+        for (var i = 0; i < graph.height; i++) left[i + 3] = left[i + 3].replace(/\d+/g, "\x1b[33m$&\x1b[0m");
 
-        output[9] = graph.status;
+        output[graph.height + 4] = graph.status;
 
         output = output.map((value, index) => left[index] + value);
         console.log(output.join`\n`);
@@ -58,12 +58,16 @@ function processData(data) {
         const args = data.split(/\s+/g).filter(arg => !arg.startsWith("-"));
         const item = args.shift();
         if (item) screen[parseInt(args.shift())].data = [];
+    } else if (data.includes("--width")) {
+        //
     } else if (data.startsWith("--graph") || data.startsWith("-g")) {
         const args = data.split(/\s+/g).filter(arg => !arg.startsWith("-"));
         screen.push({
             name: args.shift() || "UNTITLED",
             type: args.shift() || "bar",
             status: args.join` `,
+            width: 120,
+            height: 30,
             data: [],
         });
     } else flow.emit("data", ...data?.split(/\s+/).map(value => parseFloat(value)));
