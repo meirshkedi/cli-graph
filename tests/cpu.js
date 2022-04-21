387773969c2ecaca48dce3a08a31c8f11c15bffa
aroary <meanchemshkedi@gmail.com>
2022-04-21 17:41:54 -0400

    renamed to `processCommand`

--- tests/cpu.js
+++ tests/cpu.js
@@ -1,5 +1,5 @@
 const os = require('os');
-const processData = require("../index.js");
+const processCommand = require("../index.js");
 
-os.cpus().forEach((cpu, id) => id < 4 && processData(`--graph CPU_${id} bar ${cpu.model} ${cpu.speed}`));
-setInterval(() => os.cpus().forEach((cpu, id) => id < 4 && processData(id + " " + cpu.times.user)), 1000);
+os.cpus().forEach((cpu, id) => id < 4 && processCommand(`--graph CPU_${id} bar ${cpu.model} ${cpu.speed}`));
+setInterval(() => os.cpus().forEach((cpu, id) => id < 4 && processCommand(id + " " + cpu.times.user)), 1000);
