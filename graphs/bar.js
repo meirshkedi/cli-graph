f00f62eccf8c05267b1f53afdfa91df94ac5f484
aroary <meanchemshkedi@gmail.com>
2022-04-21 14:04:53 -0400

    multiple graphs

---
+++ graphs/bar.js
@@ -0,0 +1,23 @@
+function bar(items, output, left) {
+    if (items.length > 40) items.shift();
+    var data = new Array(5).fill("");
+    var lowest = Math.floor(Math.min(...items));
+    var highest = Math.floor(Math.max(...items));
+    var range = highest - lowest;
+    var row = range / 5;
+    items.forEach((value, index) => {
+        var height = Math.floor((value - lowest) / row);
+        for (var i = 0; i < 5; i++) {
+            if (i <= height) data[i] += "█";
+            else data[i] += " ";
+        }
+    });
+    data.reverse().map(value => value.length < 40 ? " ".repeat(40 - value.length) + value : value).forEach((value, index) => {
+        left[index + 3] = ["╞", (Math.round(row) * (data.length - index)).toString(), "╡"];
+        output[index + 3] = "\x1b[36m" + value + "\u001b[0m║";
+    });
+
+    return { output, left };
+}
+
+module.exports = bar;
--- index.js
+++ index.js
@@ -3,74 +3,71 @@ const readline = require("readline");
 
 const line = readline.createInterface({ input: process.stdin, output: process.stdout });
 
-const screen = [
-    {
-        name: "TEST of BAR graph",
-        type: "bar",
-        data: []
-    }
-];
+const screen = [];
 
 const flow = new event.EventEmitter();
 
 flow.on("data", (id, item) => {
+    if (id > screen.length - 1) return console.log("\x1b[31mInvalid ID\u001b[0m");
     screen[id].data.push(item);
 
     // Clear the screen
     console.clear();
 
-    var output = new Array(10).fill("");
-    var left = new Array(10).fill([]);
+    screen.forEach((graph, id) => {
+        if (!graph.data.length) return;
+        var output = new Array(10).fill("");
+        var left = new Array(10).fill([]);
 
-    // Format left side
-    left[1] = ["╤", "A", "╒"];
-    left[2] = ["╞", "═", "╪"];
-    left[8] = ["╘", "═", "╧"];
+        // Initiate left side
+        left[1] = ["╤", "ABCD"[id], "╒"];
+        left[2] = ["╞", "═", "╪"];
+        left[8] = ["╘", "═", "╧"];
 
-    // Format output
-    output[1] += `═\x1b[32m${screen[0].name}\x1b[0m${"═".repeat(39 - screen[0].name.length)}╗`;
-    output[2] += "═".repeat(40) + "╣";
-    output[8] += "═".repeat(40) + "╝";
-    if (screen[0].type === "bar") {
-        if (screen[0].data.length > 40) screen[0].data.shift();
-        var data = new Array(5).fill("");
-        var lowest = Math.floor(Math.min(...screen[0].data));
-        var highest = Math.floor(Math.max(...screen[0].data));
-        var range = highest - lowest;
-        var row = range / 5;
-        screen[0].data.forEach((value, index) => {
-            var height = Math.floor((value - lowest) / row);
-            for (var i = 0; i < 5; i++) {
-                if (i <= height) data[i] += "█";
-                else data[i] += " ";
-            }
-        });
-        data.reverse().map(value => value.length < 40 ? " ".repeat(40 - value.length) + value : value).forEach((value, index) => {
-            left[index + 3] = ["╞", (Math.round(row) * (data.length - index)).toString(), "╡"];
-            output[index + 3] = "\x1b[36m" + value + "\u001b[0m║";
-        });
-    };
+        // Format output
+        output[1] += `═\x1b[32m${graph.name}\x1b[0m${"═".repeat(39 - graph.name.length)}╗`;
+        output[2] += "═".repeat(40) + "╣";
+        output[8] += "═".repeat(40) + "╝";
 
-    //
-    var width = Math.max(...left.map(value => value.join``.length - 2));
-    left.forEach((value, index) => index > 0 && index < 9 && (left[index] = `${left[index][0]}${"═".repeat(width - left[index][1].length)}${left[index].slice(1).join``}`));
-    left[1] = left[1].split``.reverse().join``.replace(/[a-z]+/i, "\x1b[32m$&\x1b[0m");
-    for (var i = 0; i < 5; i++) left[i + 3] = left[i + 3].replace(/\d+/g, "\x1b[33m$&\x1b[0m");
+        if (graph.type === "bar") {
+            // Format bar graph
+            var data = require("./graphs/bar")(graph.data, output, left);
+            output = data.output;
+            left = data.left;
+        };
 
-    output = output.map((value, index) => left[index] + value);
-    console.log(output.join`\n`);
+        // Format left side
+        var width = Math.max(...left.map(value => value.join``.length - 2));
+        left.forEach((value, index) => index > 0 && index < 9 && (left[index] = `${left[index][0]}${"═".repeat(width - left[index][1].length)}${left[index].slice(1).join``}`));
+        left[1] = left[1].split``.reverse().join``.replace(/[a-z]+/i, "\x1b[32m$&\x1b[0m");
+        for (var i = 0; i < 5; i++) left[i + 3] = left[i + 3].replace(/\d+/g, "\x1b[33m$&\x1b[0m");
+
+        output = output.map((value, index) => left[index] + value);
+        console.log(output.join`\n`);
+    });
 });
 
 function getInput() {
     line.question("", data => {
-        if (data === "exit") process.exit();
-        flow.emit("data", 0, ...data.split(/\s+/).map(value => Number.isNaN(value) ? 0 : parseFloat(value)));
+        console.clear();
+
+        // Use arguments
+        if (data.includes("--exit") || data.includes("-e")) process.exit();
+        else if (data.includes("--clear") || data.includes("-c")) screen[0].data = [];
+        else if (data.startsWith("--graph") || data.startsWith("-g")) {
+            const args = data.split(/\s+/g).filter(arg => !arg.startsWith("-"));
+            screen.push({
+                name: args.shift(),
+                type: args.shift(),
+                data: [...args.map(value => parseInt(value))]
+            });
+        } else flow.emit("data", ...data.split(/\s+/).map(value => parseFloat(value)));
+
+        // Continue to get input
         getInput();
     });
 };
 
 // Start
 console.clear();
-
-// Input loop
 getInput();
