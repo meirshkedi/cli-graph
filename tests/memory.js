ef7a664dd59b51a04f28b92524f238ac034998bf
aroary <meanchemshkedi@gmail.com>
2022-04-24 11:19:21 -0400

    memory test

--- tests/memory.js
+++ tests/memory.js
@@ -1,5 +1,5 @@
 const os = require('os');
 const Graph = require("../module").Graph;
-const graph = new Graph({ name: "MEMORY", type: "bar", width: 100, heigh: 20, status: "data visualization test" });
-
-setInterval(() => graph.send(os.totalmem() - os.freemem()), 1000);
+const a = new Graph({ name: "MEMORY", type: "bar", width: 100, heigh: 20, status: "data visualization test" });
+const b = new Graph({ name: "MEMORY", type: "plot", width: 100, heigh: 20, status: "data visualization test" });
+setInterval(() => [a, b].forEach(g => g.send(os.totalmem() - os.freemem())), 1000);
