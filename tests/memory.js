96b3b85ced8b37c1b5da21cf95126613a76b1f07
aroary <meanchemshkedi@gmail.com>
2022-04-24 13:29:13 -0400

    test from chiled_process

--- tests/memory.js
+++ tests/memory.js
@@ -1,5 +1,5 @@
 const os = require('os');
 const Graph = require("../module").Graph;
-const a = new Graph({ name: "MEMORY", type: "bar", width: 100, heigh: 20, status: "data visualization test" });
-const b = new Graph({ name: "MEMORY", type: "plot", width: 100, heigh: 20, status: "data visualization test" });
+const a = new Graph({ name: "MEMORY", type: "bar", width: 60, heigh: 10, status: "data visualization test" });
+const b = new Graph({ name: "MEMORY", type: "plot", width: 60, heigh: 10, status: "data visualization test" });
 setInterval(() => [a, b].forEach(g => g.send(os.totalmem() - os.freemem())), 1000);
---
+++ tests/test.js
@@ -0,0 +1,4 @@
+const chiled_process = require('child_process');
+
+if (process.argv[2] === "test") console.log("Invalid test file");
+else chiled_process.execSync(`start node tests/${process.argv[2]}.js`);
