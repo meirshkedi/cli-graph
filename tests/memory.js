f0bf21a53a4674f9810ba03461a3a86204691457
aroary <meanchemshkedi@gmail.com>
2022-04-24 11:00:10 -0400

    integrated wrapper module

--- tests/memory.js
+++ tests/memory.js
@@ -1,5 +1,5 @@
 const os = require('os');
-const processCommand = require("../index.js");
+const Graph = require("../module").Graph;
+const graph = new Graph({ name: "MEMORY", type: "bar", width: 100, heigh: 20, status: "data visualization test" });
 
-processCommand("--graph MEMORY bar data visualization test", "--width 1 100", "--height 1 20");
-setInterval(() => processCommand(`1 ${os.totalmem() - os.freemem()}`), 1000);
+setInterval(() => graph.send(os.totalmem() - os.freemem()), 1000);
